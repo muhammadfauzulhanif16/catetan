d35bb9382149d79d2170cda5dc06d37dad221c6f
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-05-30 23:58:02 +0700

    add language

---
+++ src/components/Language.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from '@chakra-ui/react'
+import { Icon } from './Icon'
+import { LocalLanguage } from '@emotion-icons/fluentui-system-regular'
+
+export const Language = () => (
+  <Button p={0} w='full'>
+    <Icon
+      initIcon={LocalLanguage}
+      iconProps={{
+        w: 6,
+        h: 6
+      }}
+    />
+  </Button>
+)
