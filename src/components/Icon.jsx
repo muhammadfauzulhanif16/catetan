99d7c7aef63a1e1b37d1388da19c0830fac35cd4
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-05-30 18:40:32 +0700

    add icon

---
+++ src/components/Icon.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Icon as IconChakra } from '@chakra-ui/react'
+
+export const Icon = ({ initIcon, finalIcon, iconProps }) => (
+  <>
+    <IconChakra
+      as={initIcon}
+      _groupHover={{ display: `${finalIcon ? 'none' : 'block'}` }}
+      {...iconProps}
+    />
+
+    {finalIcon
+      ? (
+      <IconChakra
+        as={finalIcon}
+        display='none'
+        _groupHover={{ display: 'block' }}
+        {...iconProps}
+      />
+        )
+      : null}
+  </>
+)
+
+Icon.propTypes = {
+  initIcon: PropTypes.elementType,
+  finalIcon: PropTypes.elementType,
+  iconProps: PropTypes.object
+}
