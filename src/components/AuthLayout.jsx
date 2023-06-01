88ed6f03f3f5ef61658b5ca36ebd4b90def42a2e
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-06-01 11:39:53 +0700

    separate auth layout

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Layout } from './Layout'
+import { Footer } from './Footer'
+import { Header } from './Header'
+import PropTypes from 'prop-types'
+
+export const AuthLayout = ({ title, children }) => {
+  return (
+    <Layout
+      title={title}
+      boxProps={{
+        display: 'flex',
+        flexDirection: 'column',
+        p: [4, 4, 8, 12]
+      }}
+    >
+      <Header />
+
+      {children}
+
+      <Footer />
+    </Layout>
+  )
+}
+
+AuthLayout.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.arrayOf(PropTypes.node).isRequired
+}
