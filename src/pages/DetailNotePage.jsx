4bb0924e17d38bf1540e47a00f97ac6457f7d123
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-06-06 18:57:00 +0700

    test

--- src/pages/DetailNotePage.jsx
+++ src/pages/DetailNotePage.jsx
@@ -8,6 +8,15 @@ import { LocaleContext } from '../context/Locale'
 import PropTypes from 'prop-types'
 import { Badge, Flex, Heading, Text } from '@chakra-ui/react'
 import { ThemeContext } from '../context/Theme'
+import { Navigation } from '../components/base/Navigation'
+import {
+  Archive as ArchiveRegular,
+  Delete as DeleteRegular
+} from '@emotion-icons/fluentui-system-regular'
+import {
+  Archive as ArchiveFilled,
+  Delete as DeleteFilled
+} from '@emotion-icons/fluentui-system-filled'
 
 export const DetailNotePage = ({ onLogOut }) => {
   const { theme } = useContext(ThemeContext)
@@ -26,6 +35,21 @@ export const DetailNotePage = ({ onLogOut }) => {
     }, 2000)
   }, [id])
 
+  const actionDetailPage = [
+    {
+      text: note?.archived ? 'Unarchive' : 'Archive',
+      initIcon: ArchiveRegular,
+      finalIcon: ArchiveFilled,
+      color: 'purple'
+    },
+    {
+      text: 'Delete',
+      initIcon: DeleteRegular,
+      finalIcon: DeleteFilled,
+      color: 'red'
+    }
+  ]
+
   if (isLoading) return <>loading</>
 
   return (
@@ -38,23 +62,42 @@ export const DetailNotePage = ({ onLogOut }) => {
         h='full'
         color={`gray.${theme === 'light' ? '600' : '300'}`}
         gap={4}
+        justifyContent='space-between'
       >
-        <Heading w='full'>{note.title}</Heading>
+        <Flex gap={4} direction='column'>
+          <Heading w='full'>{note.title}</Heading>
 
-        <Flex
-          justifyContent='space-between'
-          w='full'
-          gap={4}
-          alignItems='center'
-        >
-          <Text color={`gray.${theme === 'light' ? '500' : '400'}`}>
-            {note.createdAt}
-          </Text>
+          <Flex
+            justifyContent='space-between'
+            w='full'
+            gap={4}
+            alignItems='center'
+          >
+            <Text color={`gray.${theme === 'light' ? '500' : '400'}`}>
+              {note.createdAt}
+            </Text>
 
-          {note.archived && <Badge colorScheme='purple'>Archived</Badge>}
+            {note.archived && <Badge colorScheme='purple'>Archived</Badge>}
+          </Flex>
+
+          <Text>{note.body}</Text>
         </Flex>
 
-        <Text>{note.body}</Text>
+        <Flex gap={4}>
+          {actionDetailPage.map((action, id) => (
+            <Navigation
+              key={id}
+              initIcon={action.initIcon}
+              finalIcon={action.finalIcon}
+              buttonProps={{
+                w: 'full',
+                variant: 'outline',
+                colorScheme: action.color
+              }}
+              text={action.text}
+            />
+          ))}
+        </Flex>
       </Flex>
 
       <NavBar />
