e198e9cf4f27eab13f707ac6c51f6035821b51c8
muhammadfauzulhanif16 <muhammadfauzulhanif2230511102@ummi.ac.id>
2023-06-06 21:14:57 +0700

    test

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -1,11 +1,15 @@
 import React, { useContext } from 'react'
 import { Box, Flex, Text } from '@chakra-ui/react'
 import { Icon } from './base/Icon'
-import { Note } from '@emotion-icons/fluentui-system-regular'
+import { Add as AddRegular, Note } from '@emotion-icons/fluentui-system-regular'
 import { ThemeContext } from '../context/Theme'
 import { LocaleContext } from '../context/Locale'
+import { Navigation } from './base/Navigation'
+import { Add as AddFilled } from '@emotion-icons/fluentui-system-filled'
+import { useNavigate } from 'react-router-dom'
 
 export const EmptyState = () => {
+  const navigate = useNavigate()
   const { theme } = useContext(ThemeContext)
   const { locale } = useContext(LocaleContext)
 
@@ -39,6 +43,27 @@ export const EmptyState = () => {
           }.`}
         </Text>
       </Box>
+
+      <Navigation
+        initIcon={AddRegular}
+        finalIcon={AddFilled}
+        iconProps={{
+          w: 6,
+          h: 6
+        }}
+        text='Add Note'
+        buttonProps={{
+          color: `gray.${theme === 'light' ? '700' : '200'}`,
+          display: 'flex',
+          gap: 4,
+          bgColor: `yellow.${theme === 'light' ? '400' : '500'}`,
+          _hover: {
+            color: `gray.${theme === 'light' ? '200' : '700'}`,
+            bgColor: `yellow.${theme === 'light' ? '500' : '400'}`,
+            onClick: () => navigate('/add')
+          }
+        }}
+      />
     </Flex>
   )
 }
